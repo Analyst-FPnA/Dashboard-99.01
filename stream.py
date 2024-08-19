4152053c41023a48b75f1aa3a30dd1cd1b747e00
Analyst-FPnA <analyst.fpna@gmail.com>
2024-08-19 13:48:48 +0700

    Update stream.py

--- stream.py
+++ stream.py
@@ -186,7 +186,7 @@ df_9901['Kode #'] = df_9901['Kode #'].astype('int64')
 df_9901 = pd.merge(df_9901, df_pic, how='left', on='Kode #').fillna('')
 df_9901 = df_9901.loc[:,['Nama Cabang','Kota/Kabupaten','Provinsi Gudang','Nomor #','Tanggal','Pemasok','Kategori Pemasok','#Group','Kode #','Nama Barang','Kategori Barang','#Purch.Qty','#Purch.UoM','#Prime.Ratio','#Prime.Qty','#Prime.UoM','#Purch.@Price','#Purch.Discount','#Prime.NetPrice','#Purch.Total','Month','PIC']]
 df_9901 = df_9901[df_9901['#Prime.NetPrice']!=0]
-st.write(df_9901['Month'].unique())
+
 db = pd.read_csv('database barang.csv')
 db = db.drop_duplicates()
 db = pd.concat([db[db['Kode #'].astype(str).str.startswith('1')].sort_values('Kode #').drop_duplicates(subset=['Kode #']),
@@ -198,6 +198,7 @@ df_test['WEIGHT AVG'] = df_test['#Purch.Total'].astype(float)/df_test['#Prime.Qt
 df_test = df_test.rename(columns={'#Prime.Qty':'QUANTITY'}).drop(columns='#Purch.Total')
 df_test = df_test.merge(db.drop_duplicates(), how='left', on='Kode #')
 df_test['Filter Barang'] = df_test['Kode #'].astype(str) + ' - ' + df_test['Nama Barang']
+st.write(df_test['Month'].unique())
 
 if cab != 'All' :
     df_test = df_test[df_test['Nama Cabang']==cab]
