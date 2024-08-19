b4909991a53af1584741fdc0cfd73ef1a3ec6269
Analyst-FPnA <analyst.fpna@gmail.com>
2024-08-19 14:00:46 +0700

    Update stream.py

--- stream.py
+++ stream.py
@@ -192,7 +192,7 @@ db = db.drop_duplicates()
 db = pd.concat([db[db['Kode #'].astype(str).str.startswith('1')].sort_values('Kode #').drop_duplicates(subset=['Kode #']),
                 db[~db['Kode #'].astype(str).str.startswith('1')]], ignore_index=True)
 df_test['#Prime.Qty'] = df_test['#Prime.Qty'].astype(float)
-df_test['#'#Purch.Total'] = df_test['#Purch.Total'].astype(float)
+df_test['#Purch.Total'] = df_test['#Purch.Total'].astype(float)
 df_test = df_9901[df_9901['PIC']==pic].groupby(['Month', 'Nama Cabang','Kode #']).agg({'#Prime.Qty': 'sum','#Purch.Total': 'sum'}).reset_index()
 
 st.write(df_test[df_test['#Purch.Total']==''])
