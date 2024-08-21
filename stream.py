51fd5c24f69681428c521766eb9e66325a403de7
Analyst-FPnA <analyst.fpna@gmail.com>
2024-08-21 15:27:35 +0700

    Update stream.py

--- stream.py
+++ stream.py
@@ -36,9 +36,7 @@ def create_sales_map_chart(df):
     )
     
     # Mengupdate peta untuk fokus ke Indonesia
-    fig.update_geos(
-    fitbounds="locations",
-    showcountries=False)
+
     
     # Menyesuaikan tampilan layout
     fig.update_layout(
