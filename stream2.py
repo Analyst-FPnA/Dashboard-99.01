bfe30c1b3b86ae9db4af0bd78e281693df4cb43c
Analyst-FPnA <analyst.fpna@gmail.com>
2024-08-22 10:19:25 +0700

    Update stream2.py

--- stream2.py
+++ stream2.py
@@ -356,7 +356,7 @@ if 'filtered_df_test' in st.session_state:
         df_prov = st.session_state.filtered_df_prov[st.session_state.filtered_df_prov['Filter Barang'].isin(barang)].groupby(['Provinsi'])[['WEIGHT AVG']].mean().reset_index()
     col = st.columns(2)
     with col[0]:
-        create_horizontal_barchart(df_prov, 'properties', f'{wa_wty}')
+        create_horizontal_barchart(df_prov.sort_values(f'{wa_qty}'), 'properties', f'{wa_wty}')
     with col[1]:
         create_sales_map_chart(prov.merge(df_prov,how='left',left_on='properties',right_on='Provinsi').drop(columns='Provinsi').fillna(0))
     st.dataframe(df_test, use_container_width=True, hide_index=True)
