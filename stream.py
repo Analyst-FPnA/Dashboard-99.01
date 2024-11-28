0adf69b4f6c543733a294498a6698df8cb3df447
Analyst-FPnA <analyst.fpna@gmail.com>
2024-11-28 14:08:57 +0700

    Update stream.py

--- stream.py
+++ stream.py
@@ -5,7 +5,7 @@ import io
 import pandas as pd
 import os
 import gdown
-import tempfile
+import tempfile 
 import matplotlib.pyplot as plt
 import seaborn as sns
 
@@ -205,7 +205,7 @@ with col[1]:
     list_bulan = [
         'January', 'February', 'March', 'April', 'May', 'June',
         'July', 'August', 'September', 'October', 'November', 'December']
-    bulan = st.multiselect("BULAN:", list_bulan, default = ['August','September','October'], on_change=reset_button_state)
+    bulan = st.multiselect("BULAN:", list_bulan, default = ['September','October','November'], on_change=reset_button_state)
     bulan = sorted(bulan, key=lambda x: list_bulan.index(x))
 
 category = st.selectbox("TOP/BOTTOM:", ['TOP','BOTTOM'], index= 0, on_change=reset_button_state)
