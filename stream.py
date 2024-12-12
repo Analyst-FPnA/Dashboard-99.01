7970d58b8ea083a0f2f8ed49b24e14d3c29ae99a
Analyst-FPnA <analyst.fpna@gmail.com>
2024-12-12 17:14:41 +0700

    Update stream.py

--- stream.py
+++ stream.py
@@ -367,7 +367,7 @@ if ('filtered_df_test' in st.session_state) :
     
     df_vendor = df_vendor[(df_vendor['Filter Barang'].isin(df_vendor['Filter Barang'].unique() if barang==['All'] else barang)) & (df_vendor['Pemasok'].isin(df_vendor['Pemasok'].unique() if vendor==['All'] else vendor))]
     df_test = df_vendor.pivot(index=['Pemasok','Kode #','Nama Barang','Filter Barang'],columns='Month',values=wa_qty).fillna('').reset_index()
-    st.dataframe(df_test)
+
     if len(bulan)>=3:
         df_test[f'Diff {bulan[-3]} - {bulan[-2]}'] = df_test.apply(lambda row: 0 if ((row[bulan[-2]] == '') or (row[bulan[-3]]=='')) else ((row[bulan[-2]] - row[bulan[-3]]) / row[bulan[-3]]), axis=1)
         df_test[f'Diff {bulan[-2]} - {bulan[-1]}'] = df_test.apply(lambda row: 0 if ((row[bulan[-1]] == '') or (row[bulan[-2]]=='')) else ((row[bulan[-1]] - row[bulan[-2]]) / row[bulan[-2]]), axis=1)
@@ -377,11 +377,6 @@ if ('filtered_df_test' in st.session_state) :
         df_test[f'Diff {bulan[-2]} - {bulan[-1]}'] = df_test.apply(lambda row: 0 if ((row[bulan[-1]] == '') or (row[bulan[-2]]=='')) else ((row[bulan[-1]] - row[bulan[-2]]) / row[bulan[-2]]), axis=1)
         df_test = df_test.sort_values(df_test.columns[-1],ascending=False)
         #df_test.loc[:,df_test.columns[-1]] = df_test.loc[:,df_test.columns[-1:]].apply(lambda x: f'{x*100:.2f}%')   
-    
-    if 'All' in barang:
-        df_test = st.session_state.filtered_df_test.drop(columns='Filter Barang')
-    if 'All' not in barang:
-        df_test = st.session_state.filtered_df_test[st.session_state.filtered_df_test['Filter Barang'].isin(barang)].drop(columns='Filter Barang')
 
     if wa_qty =='WEIGHT AVG':
         df_test.loc[:,[x for x in df_test.columns if x in list_bulan]] = df_test.loc[:,[x for x in df_test.columns if x in list_bulan]].applymap(lambda x: f'{x:.2f}' if isinstance(x, float) else x)
